7d1a0e4880f0cb94b198f457597f74168f049c52
joako <joako.daurat.com>
2022-10-25 17:54:13 +0200

    captcha

--- enviarmail.php
+++ enviarmail.php
@@ -6,6 +6,14 @@ error_reporting( E_ALL );
 $nombre = $_POST['nombre'];
 $email = $_POST['email'];
 
+$ip = $_SERVER['REMOTE_ADDR'];
+$captcha = $_POST['g-recaptcha-response'];
+$secre = "6Ld8KLAiAAAAAOmMxyxKCwIfQ9PgvnSl2-xogOSG";
+
+$respuesta = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secre&response=$captcha&remoteip=$ip");
+
+$atributos = json_decode($respuesta,TRUE);
+
 
 $from = "vero@veromagicstone.com";
 $to = "joaquin.daurat@gmail.com";
@@ -33,14 +41,22 @@ if ($_POST['stopspam'] != ""){
  exit();
 
 } else {
-  
-   // Es un usuario real, proceder a enviar el formulario.
-   if(mail($to,$subject,$message, $headers)) {
+  // verifico que el captcha este bien
+    if(!$atributos['success']){
+      echo '<script language="javascript">alert("ingrese bien el captcha");</script>';
 
-    header("Location: index.html#contact");
-    } else {
-    echo "Message was not sent.";
-      }
+    }else{ 
+        // Es un usuario real, proceder a enviar el formulario.
+        if(mail($to,$subject,$message, $headers)) {
+          header("Location: index.html#contact");
+          } else {
+              echo "Message was not sent.";
+           }
+
+
+    }
+  
+
 }
 
 
